f8a8ef4fce8b89dcedc3d3d82f2f2ce79cca0be0
Tomas Campusano <tomascampusano.10@gmail.com>
2022-02-10 18:03:14 -0300

    Creada carpeta views

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navigation from "./components/layout/Navigation";
-import Home from "./components/Home/Home.js";
+import Home from "./components/views/Home/Home.js";
 import Footer from "./components/layout/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
