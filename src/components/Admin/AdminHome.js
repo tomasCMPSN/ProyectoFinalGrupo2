9a4c6d9abad7080eec091e6b2abf62e414d62b93
Ezequiel Garcia <ezequielgar@gmail.com>
2022-02-10 13:55:38 -0300

    creacion de componentes admin

--- src/components/Admin/AdminHome.js
+++ src/components/Admin/AdminHome.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Card from './Cards/Card'
 
-const Admin = () => {
+const AdminHome = () => {
     return (
-        <div >
+        <div>
             <h1>hola</h1>
+            
         </div>
     );
 };
 
-export default Admin;
+export default AdminHome;
