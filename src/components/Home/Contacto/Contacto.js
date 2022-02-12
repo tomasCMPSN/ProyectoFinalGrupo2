c7b3de248fb53999f0991182c3e2372201a081da
luchyb10@gmail.com <luchyb10@gmail.com>
2022-02-12 19:56:39 -0300

    maquetado de componente contacto

--- src/components/Home/Contacto/Contacto.js
+++ src/components/Home/Contacto/Contacto.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import { Form } from "react-bootstrap";
 
 const Contacto = () => {
     return (
         <div>
+        <Form className="my-5 container">
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label className="fw-bold">Nombre y Apellido</Form.Label>
+            <Form.Control type="text" placeholder="Nombre y Apellido" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label className="fw-bold">Correo Electronico</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="email@ejemplo.com"
+              required
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label className="fw-bold">Dejanos tu Consulta</Form.Label>
+            <Form.Control as="textarea" rows={3} />
+          </Form.Group>
+          <button className="btn btn-primary" type="submit">
+            Enviar
+          </button>
+        </Form>
+        
             
-        </div>
+    
+      </div>
     );
 };
 
