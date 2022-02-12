c09c078cb7ae5d4d50ab947bdb8808b145a62528
Ezequiel Garcia <ezequielgar@gmail.com>
2022-02-12 14:42:12 -0300

    creacion de path

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navigation from "./components/layout/Navigation";
 import Home from "./components/Home/Home.js";
+import AdminHome from "./components/Admin/AdminHome";
 import Footer from "./components/layout/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <main>
           <Routes>
             <Route exact path="/" element={ <Home /> }/>
+            <Route exact path="/admin" element={ <AdminHome /> }/>
+            
           </Routes>
         </main>
         <Footer />
