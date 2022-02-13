7dc68d2e5f32a3b22fb66904b0c6fbb6529b2c04
Ezequiel Garcia <ezequielgar@gmail.com>
2022-02-13 18:48:08 -0300

    finish

--- src/components/Admin/AdminHome.js
+++ src/components/Admin/AdminHome.js
@@ -1,17 +1,15 @@
-import React from 'react';
+import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import Cards from './Card/Cards'
-import WelcomeToast from './Card/WelcomeToast'
+import Cards from "./Card/Cards";
+import WelcomeToast from "./Card/WelcomeToast";
 
 const AdminHome = () => {
   return (
-    
-      <Container>
-        <WelcomeToast/>
-        <Cards></Cards>
+    <Container>
+      <WelcomeToast />
 
-      </Container>
-    
+      <Cards></Cards>
+    </Container>
   );
 };
 
--- src/components/Admin/Card/WelcomeToast.jsx
+++ src/components/Admin/Card/WelcomeToast.jsx
@@ -3,8 +3,11 @@ import { Col, Container, Row, Button } from "react-bootstrap";
 import Toast from "react-bootstrap/Toast";
 import ToastContainer from "react-bootstrap/ToastContainer";
 
+
 const WelcomeToast = () => {
   const [show, setShow] = useState(false);
+  const [showA, setShowA] = useState(true);
+  const toggleShowA = () => setShowA(!showA);
 
   return (
     // <Row>
@@ -35,15 +38,15 @@ const WelcomeToast = () => {
     //     <Button onClick={() => setShow(true)}>Show Toast</Button>
     //   </Col>
     // </Row>
-    <ToastContainer className="p-2" position="bottom-end">
+    <ToastContainer className="p-2" position="bottom-center">
 
-    <Toast>
+<Toast show={showA} delay={3000} autohide onClose={toggleShowA} className="bg-success">
   <Toast.Header>
     <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
-    <strong className="me-auto">Bootstrap</strong>
-    <small>11 mins ago</small>
+    <strong className="me-auto ">Bienvenido</strong>
+    <small>Paws & Claws</small>
   </Toast.Header>
-  <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+  <Toast.Body className="text-white">Iniciaste sesión como Administrador</Toast.Body>
 </Toast>
     </ToastContainer>
   );
