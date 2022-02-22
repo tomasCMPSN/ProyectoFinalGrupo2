328b0a41b99d16f9cc751ecbcd3dd61767c858cd
natalia sanchez <nataliasanchezmkt@gmail.com>
2022-02-22 20:16:09 -0300

    merge con rama adminTurnos

--- src/App.js
+++ src/App.js
@@ -2,60 +2,71 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navigation from "./components/layout/Navigation";
 import Home from "./components/views/Home/Home";
-import AdminHome from "./components/views/Admin/AdminHome"
+import AdminHome from "./components/views/Admin/AdminHome";
 import Footer from "./components/layout/Footer";
 import Login from "./components/views/Home/Login/Login";
-import Contacto from "./components/views/Home/Contacto/Contacto"
+import Contacto from "./components/views/Home/Contacto/Contacto";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Turnos from './components/views/turnos/TurnosCreate'
-import TurnosTable from './components/views/turnos/TurnosTable'
-import TurnosEdit from './components/views/turnos/TurnosEdit'
-import {useState,useEffect} from 'react'
-
-
+import TurnosCreate from "./components/views/turnos/TurnosCreate";
+import TurnosTable from "./components/views/turnos/TurnosTable";
+import TurnosEdit from "./components/views/turnos/TurnosEdit";
+import { useState, useEffect } from "react";
 
 function App() {
   //  states para la logica
 
-   const [turnos, setTurno] = useState([]);
-  
-  useEffect(()=>{
+  const [turnos, setTurnos] = useState([]);
+  const DB = process.env.REACT_APP_APPI_APPOINTMENT;
 
+  useEffect(() => {
     getApi();
-  },[])
+  }, []);
 
-const DB = process.env.REACT_APP_APPI_APPOINTMENT;
-
-   const getApi =async () =>{
-   try {
-
-     const res = await fetch(DB)
-     const appApi =  await res.json();
-     setTurno(appApi)
-    
-      
-   } catch (error) {
-     console.log(error)
-      
-   }
-
- }
+  const getApi = async () => {
+    try {
+      const res = await fetch(DB);
+      const appApi = await res.json();
+      setTurnos(appApi);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div>
       <BrowserRouter>
         <Navigation />
-      
+
         <main>
           <Routes>
-            <Route exact path="/" element={ <Home /> }/>
-            <Route exact path="/login" element ={<Login />}/>
-            <Route exact path="/contacto" element ={<Contacto />}/>
-            <Route exact path="/admin" element={ <AdminHome /> }/>
-            <Route exact path="/turnoscreate" element={ <Turnos /> }/>
-            <Route exact path="/turnostable" element={ <TurnosTable turnos={turnos} /> }/>
-            <Route exact path="/turnosedit" element={ <TurnosEdit /> }/>
-            
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/contacto" element={<Contacto />} />
+            <Route exact path="/admin" element={<AdminHome />} />
+            <Route exact path="/turnoscreate" element={<Turnos />} />
+            <Route
+              exact
+              path="/turnostable"
+              element={<TurnosTable turnos={turnos} />}
+            />
+            <Route exact path="/turnosedit" element={<TurnosEdit />} />
+
+        
+            <Route
+              exact
+              path="/turnoscreate"
+              element={<TurnosCreate DB={DB} getApi={getApi} />}
+            />
+            <Route
+              exact
+              path="/turnostable"
+              element={<TurnosTable turnos={turnos} DB={DB} getApi={getApi} />}
+            />
+            <Route
+              exact
+              path="/turnosedit/:id"
+              element={<TurnosEdit DB={DB} getApi={getApi} />}
+            />
           </Routes>
         </main>
         <Footer />
