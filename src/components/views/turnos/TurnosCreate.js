a103bfed40e6d2d8a85dd82a6e0c4ef5aa147643
luchyb10@gmail.com <luchyb10@gmail.com>
2022-02-23 17:45:25 -0300

    cambios en TurnosCreate

--- src/components/views/turnos/TurnosCreate.js
+++ src/components/views/turnos/TurnosCreate.js
@@ -1,81 +1,122 @@
 import React, { useState } from "react";
 import { Form } from "react-bootstrap";
-import {validateNames, validateVet, validateDni, validateSpecie} from '../../helpers/ValidateForms'
-import './Turnos.css'
+import {
+  validateNames,
+  validateVet,
+  validateDate,
+} from "../../helpers/ValidateForms";
+import "./Turnos.css";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 
-const Turnos = () => {
+const TurnosCreate = ({ DB, getApi }) => {
   // States
 
-  const [petName, setPetName]= useState('');
-  const [name, setName]= useState('')
-  const [dni, setDni]= useState('')
-  const [vet, setVet]= useState('');
-  const [specie, setSpecie]= useState('');
+  const [petName, setPetName] = useState("");
+  const [vet, setVet] = useState("");
+  const [date, setDate] = useState("");
+
+  //Use navigate
+  const navigate = useNavigate();
 
   //Funcion para crear un turno
-const handleSubmit = (e)=>{
-  e.preventDefault()
-  // validar datos
-  if(validateNames(petName) && validateNames(name) && validateDni(dni)&& validateVet(vet) && validateSpecie(specie) ){
-    console.log('todo pasa')
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // validar datos
+    if (!validateNames(petName) || !validateVet(vet) || !validateDate(date)) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Ingreso algun dato incorrecto, por favor revise el formulario",
+      });
+      return;
+    }
 
-  }else{alert('no pasa')}
+    // Enviamos los datos para gaurdarlos
 
+    const newAppointment = {
+      petName,
 
-}
+      vet,
 
-  
+      date,
+    };
 
-
+    Swal.fire({
+      title: "Esta seguro que quiere asignar este turno?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#56ced6",
+      cancelButtonColor: "#ffc872",
+      cancelButtonText: "Cancelar",
+      confirmButtonText: "Si",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          const res = await fetch(DB, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newAppointment),
+          });
+          
+          console.log(res)
+          if (res.status === 201) {
+            Swal.fire(
+              "Creado!",
+              "Su turno fue asignado correctamente",
+              "success"
+            );
+            getApi();
+            navigate("../turnostable");
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    });
+  };
   return (
-    <section className='container mt-5  '>
-        <article className="text-center" >
-        <h1 className='form-style-title '>Administrador de turnos 📝</h1>
-        </article>
-        
-     
-     <article className='d-flex justify-content-center mb-5 ' >
+    <section className="container mt-5 ">
+      <article>
+        <h1 className="form-style-title ">Administrador de turnos 📝</h1>
+      </article>
 
+      <article className="d-flex justify-content-center mb-5 ">
         <Form className="mb-5 form_style" onSubmit={handleSubmit}>
           <Form.Group className="mb-3 " controlId="formBasicEmail">
-            <Form.Label>🐶Nombre del paciente*</Form.Label>
-            <input className='form-stle-inner' type="text"  placeholder="Puki" onChange={(e)=>setPetName(e.target.value)}/>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>👨🏽‍🎤Nombre del dueño *</Form.Label>
-            <input  type="text" placeholder="Lucas" className='form-stle-inner' onChange={({target})=>setName(target.value)}/>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>👨🏽‍🎤DNI *</Form.Label>
-            <input  type="text" placeholder="Lucas" className='form-stle-inner' onChange={({target})=>setDni(target.value)}/>
+            <Form.Label>🐶 Nombre del paciente*</Form.Label>
+            <input
+              className="form-stle-inner"
+              type="text"
+              placeholder="Ingrese nombre de la mascota"
+              onChange={(e) => setPetName(e.target.value)}
+            />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Label >👩🏻‍⚕️Veterinario*</Form.Label>
-            <select className='form-stle-inner' onChange={({target})=>setVet(target.value)} >
+            <Form.Label>👩🏻‍⚕️ Veterinario*</Form.Label>
+            <select
+              className="form-stle-inner"
+              onChange={({ target }) => setVet(target.value)}
+            >
               <option value="">Seleccione un profesional </option>
-              <option value="emilse">Dra. Emilse Arias</option>
-              <option value="jony">Dr. Jonathan Plodzien </option>
+
+              <option value="Dr Jorge Ignacio">Dr. Jorge Ignacio</option>
+              <option value="Dra Marta Minujin">Dra. Marta Minujin </option>
             </select>
           </Form.Group>
-         
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Label>🐰Especie*</Form.Label>
-            <select className='form-stle-inner' onChange={({target})=>setSpecie(target.value)} >
-              <option value="">Seleccione una especie</option>
-              <option value="ave">Ave</option>
-              <option value="gato">Gato</option>
-              <option value="perro">Perro</option>
-              <option value="pez">Pez</option>
-              <option value="reptil">Reptil</option>
-              <option value="roedor">Roedor</option>
-            </select>
-          </Form.Group>
-          {/* <Form.Group className="mb-3"  controlId="formBasicEmail">
-            <Form.Label>📅 Fecha </Form.Label>
-            <input className='form-stle-inner' type="date" />
-          </Form.Group>
+
           <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>📅 Fecha </Form.Label>
+            <input
+              className="form-stle-inner"
+              placeholder="Ingrese la fecha para el turno dd/mm/yyyy"
+              onChange={({ target }) => setDate(target.value)}
+            />
+          </Form.Group>
+          {/* <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>⏰ Horario </Form.Label>
             <select className='form-stle-inner' >
               <option value="">Seleccione un horario</option>
@@ -104,15 +145,13 @@ const handleSubmit = (e)=>{
            
           </Form.Group> */}
 
-        
-        <div className="text-center mt-4">
+          <div className="text-center mt-4">
             <button className="form-style-btn ">Cargar 🐾</button>
           </div>
         </Form>
-        </article>
-     
+      </article>
     </section>
   );
 };
 
-export default Turnos;
+export default TurnosCreate;
