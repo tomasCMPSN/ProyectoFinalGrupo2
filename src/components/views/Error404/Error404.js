fbdfc096ab5a190b7e222242db1f79ed86d2bc7b
natalia sanchez <nataliasanchezmkt@gmail.com>
2022-02-23 18:57:50 -0300

    cambios

---
+++ src/components/views/Error404/Error404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Image } from 'react-bootstrap';
+
+const Error404 = () => {
+    return (
+        <div>
+            <Image src='images/404.png' className='error img-responsive'  alt='' />
+        </div>
+    );
+};
+
+export default Error404;
