efe142fcffc6a43b4a65462b0d6c58ebafd0718b
luchyb10@gmail.com <luchyb10@gmail.com>
2022-02-28 16:59:23 -0300

    corrección de datlles de servicios

--- src/components/views/Home/Clima/Clima.js
+++ src/components/views/Home/Clima/Clima.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Row, Col, Container } from "react-bootstrap";
+import "./clima.css"
 
 const Clima = () => {
   const [clima, setClima] = useState([]);
--- src/components/views/Home/Servicios/Servicios.js
+++ src/components/views/Home/Servicios/Servicios.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import { Row, Col, Card, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./Servicios.css"
 
@@ -7,20 +7,18 @@ import "./Servicios.css"
 const Servicios = () => {
     return (
        
-      <Row className="bg-primary">
+      <Row >
         <Col sm={12} md={6} className="mb-2">
         <div className="d-flex justify-content-center">
           <Card className="text-center sombras">
             <Card.Body>
             <Card.Img
               variant="top"
-              src="https://st2.depositphotos.com/1063397/5982/i/600/depositphotos_59823897-stock-photo-bathing-a-dog-golden-retriever.jpg"
+              src="https://i.pinimg.com/originals/73/ed/68/73ed68a63912b33a46a4a2aefed2dce6.jpg"
               alt='gatito tierno de Primeros pasos'
             />
-              <Card.Title className='mt-2'>Corte</Card.Title>
-              <Card.Text>
-                hdfjhfjhf
-              </Card.Text>
+                <Card.Title className="letra m-3"> <h4> Baño </h4></Card.Title>
+             
               <Link to='*' variant="light" >
               
               <button className="btn"> Ver más🐾</button>
@@ -29,7 +27,7 @@ const Servicios = () => {
           </Card>
           </div>
         </Col>
-        <Col sm={12} md={6} className="mb-2 bg-danger">
+        <Col sm={12} md={6} className="mb-2 ">
         <div className="d-flex justify-content-center">
           <Card className="text-center  sombras">
             <Card.Body>
@@ -38,20 +36,24 @@ const Servicios = () => {
               src="https://animalesmascotas.com//wp-content/uploads/2009/11/cortar-pelo-mascota.jpg"
               alt='perro mirando al frente del Plan Madurando'
             />
-              <Card.Title className='mt-2'>Corte</Card.Title>
-              <Card.Text>
-                gdgdsgsg
-              </Card.Text>
+            <Card.Title className="letra m-3"> <h4> Corte </h4></Card.Title>
+             
               <Link to='*' variant="light" >
-              
               <button className="btn"> Ver más🐾</button>
               </Link>
             </Card.Body>
           </Card>
           </div>
         </Col>
+        <div className="mt-5 text-center">
+        <Link to="*">
+          <button className="btn btn-large "> <h4>Ver más servicios🐾</h4></button>
+        </Link>
+      </div>
+      
         </Row>
-    
+        
+         
     );
 };
 
